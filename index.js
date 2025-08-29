b28bbe8135b899d60d594a68ca5479dafd257972
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-08-29 11:31:54 -0500

    Remove cors dependancy

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
 let notes = [
@@ -21,7 +20,6 @@ let notes = [
 ]
 
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
 
 app.get('/', (request, response) => {
